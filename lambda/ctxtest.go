445fd1c6dd6c93c838b7643b7252b5fbe3ca91ab
bakiss <balazs.2.kiss@nokia.com>
2026-02-17 08:12:28 +0000

    get post form url encoded data from request body

--- lambda/ctxtest.go
+++ lambda/ctxtest.go
@@ -6,8 +6,11 @@ package lambda
 
 import (
 	"context"
+	"io"
 	"net/http"
 	"net/url"
+	"strings"
+	"testing"
 )
 
 type testFakeWriter struct {
@@ -44,4 +47,33 @@ func NewTestCtx(method string, rawurl string, header http.Header, vars map[strin
 // Usually used on testing.
 func (l *Lambda) ResponseHeader() http.Header {
 	return l.w.Header()
+}
+
+func TestRequestBodyQueryParameter_FormURLEncoded(t *testing.T) {
+	body := "foo=bar&num=123&multi=a&multi=b"
+
+	req, err := http.NewRequest(http.MethodPost, "http://example.com/test", io.NopCloser(strings.NewReader(body)))
+	if err != nil {
+		t.Fatalf("NewRequest: %v", err)
+	}
+	req.Header = make(http.Header)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	l := newLambda(newTestFakeWriter(), req, nil)
+	values := l.RequestBodyQueryParameter()
+	if values == nil {
+		t.Fatalf("expected non-nil values")
+	}
+
+	if got := values.Get("foo"); got != "bar" {
+		t.Fatalf("foo: got %q, want %q", got, "bar")
+	}
+	if got := values.Get("num"); got != "123" {
+		t.Fatalf("num: got %q, want %q", got, "123")
+	}
+
+	multi := values["multi"]
+	if len(multi) != 2 || multi[0] != "a" || multi[1] != "b" {
+		t.Fatalf("multi: got %#v, want []string{\"a\",\"b\"}", multi)
+	}
 }
